d3fb69f0e889d319bf3832c18bdc5db12b43904e
Akiva Green <green.akiva@gmail.com>
2025-03-26 00:42:28 -0400

    Updated the basics for the data cleaner lwc

--- force-app/main/default/lwc/dlc_DataLoadCleaner/dlc_DataLoadCleaner.js
+++ force-app/main/default/lwc/dlc_DataLoadCleaner/dlc_DataLoadCleaner.js
@@ -1,10 +1,25 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire, track } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { getRecord, notifyRecordUpdateAvailable } from 'lightning/uiRecordApi';
 import LightningConfirm from 'lightning/confirm';
+
+import USER_ID from '@salesforce/user/Id';
+import NAME_FIELD from '@salesforce/schema/User.Name';
 
 export default class Dlc_DataLoadCleaner extends LightningElement {
 
-    
+    @api recordId;
+    dataLoad;
+    loadingSpinner = true;
+    showFileInfo = false;
+    file = [];
+
+    showLandingPage = true;
+    showNewDataLoadPage = false;
+    showLoadDataLoadPage = false;
+    showFieldMappingPage = false;
+    showPicklistMappingPage = false;
+    showSummaryPage = false;
     
 }
